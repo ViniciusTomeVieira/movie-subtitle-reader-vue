68ac55926f8481e5dfaf6db2d5e5426eceb00688
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2021-05-11 16:54:34 -0300

    chore: Create the pill tags with the most used words

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <v-main fluid>
+      <v-form>
+          <v-file-input label="Selecione as legendas" outlined
+           append-outer-icon="mdi-send" prepend-icon="mdi-message-text"
+            multiple chips v-model="files"
+           @click:append-outer="processSubtitles">
+
+          </v-file-input>
+      </v-form>
       <div class="pills">
           <Pill v-for="word in groupedWords" :key="word.name"
             :name="word.name" :amount="word.amount" />
@@ -13,11 +21,17 @@ export default {
     components: { Pill },
     data: function() {
         return {
+            files: [],
             groupedWords: [
                 { name: 'i', amount: 1234 },
                 { name: 'you', amount: 900 },
-                { name: 'he', amount: 853 }
+                { name: 'he', amount: 853 }          
             ]
+        }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
         }
     }
 
@@ -25,5 +39,9 @@ export default {
 </script>
 
 <style>
-
+    .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
 </style>
